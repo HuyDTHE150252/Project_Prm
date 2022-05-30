32707f7caaf57e77bee3d02d93c2b090964aae81
DuongTrungHuy <huydthe150252@fpt.edu.vn>
2022-05-31 02:31:31 +0700

    Testing

---
+++ app/src/main/java/com/fptu/android/project/interfaces/UserInterface.java
@@ -0,0 +1,8 @@
+package com.fptu.android.project.interfaces;
+
+import com.fptu.android.project.model.User;
+
+public interface UserInterface {
+    public void doLogin(User user);
+
+}
---
+++ app/src/main/java/com/fptu/android/project/service/UserService.java
@@ -0,0 +1,11 @@
+package com.fptu.android.project.service;
+
+import com.fptu.android.project.interfaces.UserInterface;
+import com.fptu.android.project.model.User;
+
+public class UserService implements UserInterface {
+    @Override
+    public void doLogin(User user) {
+        
+    }
+}
