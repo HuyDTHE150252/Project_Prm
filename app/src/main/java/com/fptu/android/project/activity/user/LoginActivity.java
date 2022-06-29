c4a0876f97183663b3a84d80553e1e6ec323abc5
megamindddd <64695437+megamindddd@users.noreply.github.com>
2022-06-30 01:40:57 +0700

    Forgot Password

--- app/src/main/java/com/fptu/android/project/activity/user/LoginActivity.java
+++ app/src/main/java/com/fptu/android/project/activity/user/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.fptu.android.project.activity.user;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,12 +10,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fptu.android.project.R;
 import com.fptu.android.project.activity.HomePageActivity;
 import com.fptu.android.project.activity.fragment.ProfileFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,7 +27,7 @@ import org.jetbrains.annotations.NotNull;
 
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btn_signup, btn_signin;
+    private Button btn_signup, btn_signin, btn_forgot;
     private EditText et_username, et_password;
     private FirebaseAuth fAuth;
     @Override
@@ -41,12 +45,54 @@ public class LoginActivity extends AppCompatActivity {
     void bindingView(){
         btn_signup=findViewById(R.id.btn_donothaveaccount);
         btn_signin=findViewById(R.id.btn_signin);
+        btn_forgot=findViewById(R.id.btn_forgot_password);
         et_username=findViewById(R.id.et_username);
         et_password=findViewById(R.id.et_password);
+
     }
     void bindingAction(){
         btn_signup.setOnClickListener(this::signup);
         btn_signin.setOnClickListener(this::signin);
+        btn_forgot.setOnClickListener(this::forgotpassword);
+    }
+
+    private void forgotpassword(View view) {
+        final EditText resetMail = new EditText(view.getContext());
+        final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+        passwordResetDialog.setTitle("Đặt lại mật khẩu ?");
+        passwordResetDialog.setMessage("Nhập địa chỉ email của bạn. " +
+                "Link đặt lại mật khẩu sẽ được gửi!");
+        passwordResetDialog.setView(resetMail);
+
+        passwordResetDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // extract the email and send reset link
+                String mail = resetMail.getText().toString();
+                fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(LoginActivity.this, "Link đặt lại mật khẩu đã được gửi. Hãy kiểm tra email.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Lỗi! Không thể gửi link xác nhận." + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        passwordResetDialog.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // close the dialog
+            }
+        });
+
+        passwordResetDialog.create().show();
+
     }
 
     private void signin(View view) {
@@ -80,9 +126,50 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
         }
 
-
+//    forgotTextLink.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//
+//            final EditText resetMail = new EditText(v.getContext());
+//            final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+//            passwordResetDialog.setTitle("Reset Password ?");
+//            passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+//            passwordResetDialog.setView(resetMail);
+//
+//            passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    // extract the email and send reset link
+//                    String mail = resetMail.getText().toString();
+//                    fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                        @Override
+//                        public void onSuccess(Void aVoid) {
+//                            Toast.makeText(Login.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }).addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//                            Toast.makeText(Login.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+//                        }
+//                    });
+//
+//                }
+//            });
+//
+//            passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    // close the dialog
+//                }
+//            });
+//
+//            passwordResetDialog.create().show();
+//
+//        }
+//    });
     private void signup(View view) {
         Intent i =new Intent(LoginActivity.this,SignupActivity.class);
         startActivity(i);
